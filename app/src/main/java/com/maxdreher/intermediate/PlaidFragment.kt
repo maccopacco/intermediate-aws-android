10103e5b22ebfa405efe2d83e98a49baf8371095
maccopacco <maxdmax101@gmail.com>
2021-03-07 22:09:46 -0500

    Plaid Api added & compiling

--- app/src/main/java/com/maxdreher/intermediate/PlaidFragment.kt
+++ app/src/main/java/com/maxdreher/intermediate/PlaidFragment.kt
@@ -1,17 +1,34 @@
 package com.maxdreher.intermediate
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import com.plaid.client.PlaidClient
+import com.plaid.client.request.AccountsBalanceGetRequest
+import com.plaid.client.response.AccountsGetResponse
+import retrofit2.Response
+
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class PlaidFragment : Fragment(R.layout.fragment_plaid) {
+
+//    private val plaidClient = ApiClient(
+//        mapOf(
+//            "clientId" to "summ",
+//            "secret" to "summ else",
+//            "plaidVersion" to "2020-09-14"
+//        )
+//    ).apply { setPlaidAdapter(ApiClient.Development) }.createService(PlaidApi::class.java)
+
+    var plaidClient = PlaidClient.newBuilder()
+        .clientIdAndSecret("nah", "nope")
+        .developmentBaseUrl()
+        .build()
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -23,8 +40,12 @@ class PlaidFragment : Fragment(R.layout.fragment_plaid) {
                 findNavController().navigate(R.id.action_plaidFragment_to_homeFragment)
             }
             findViewById<Button>(R.id.trigger_plaid).setOnClickListener {
-                result.text = "Kinda random: ${"%3.2f".format(Math.random())}"
-                Toast.makeText(activity, "Toast!", Toast.LENGTH_LONG).show()
+//                val accessToken =
+//                    AccountsBalanceGetRequest().accessToken("bad_boy_dont_post_the_dang_keys")
+//                val response: Response<AccountsGetResponse> =
+//                    plaidClient.accountsBalanceGet(accessToken).execute()
+//
+//                result.text = if (response.isSuccessful) response.body().toString() else "Error?"
             }
         }
     }
